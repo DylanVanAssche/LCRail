6393448a52fdb3ef713ef5099992d30712ca0974
Dylan Van Assche <dylan.van.assche@protonmail.com>
2019-04-03 21:15:34 +0200

    Generated timeline + clean up

--- benchmark/plot.py
+++ benchmark/plot.py
@@ -21,12 +21,16 @@ class BaseParser:
 
     def convert_timestamps(self):
         # Convert timestamps to timeline
-        self._timeline.append((t1 - t2).seconds)
-        for t in range(1, len(self._timestamps)):
-            t1 = self._timestamps[t]
-            t2 = self._timestamps[t - 1]
-            self._timeline.append((t1 - t2).seconds)
-        #print("Timeline: {}".format(self._timeline))
+        if not self._timestamps:
+            return
+
+        begin_timestamp = self._timestamps[0]
+        for t in self._timestamps:
+            self._timeline.append(t - begin_timestamp)
+
+    @property
+    def timeline(self):
+        return self._timeline
 
 class NethogsParser(BaseParser):
     def __init__(self, input_file, process):
@@ -39,12 +43,12 @@ class NethogsParser(BaseParser):
             try:
                 if self._process in line:
                     day, month, date, time, timezone, year, process, sent, received = line.split()
-                    self._timestamps.append(datetime.strptime(time, "%H:%M:%S"))
+                    self._timestamps.append(datetime.strptime(time, "%H:%M:%S").timestamp())
                     self._sent.append(float(sent))
                     self._received.append(float(received))
             except Exception as e:
                 print("ERROR: {} for parsing line: {}".format(e, line))
-        #self.convert_timestamps()
+        self.convert_timestamps()
         print("Nethogs lines: {}".format(len(self._lines)))
 
     @property
@@ -66,13 +70,13 @@ class TopParser(BaseParser):
             try:
                 if self._process in line:
                     day, month, date, time, timezone, year, pid, user, priority, nice, virtual_mem, res, shr, state, cpu, mem, cputime, command = line.split()
-                    self._timestamps.append(datetime.strptime(time, "%H:%M:%S"))
+                    self._timestamps.append(datetime.strptime(time, "%H:%M:%S").timestamp())
                     self._cpu.append(float(cpu))
                     self._mem.append(float(mem))
                     print(self._cpu)
             except Exception as e:
                 print("ERROR: {} for parsing line: {}".format(e, line))
-        #self.convert_timestamps()
+        self.convert_timestamps()
         print("Top lines: {}".format(len(self._lines)))
 
     @property
@@ -84,24 +88,24 @@ class TopParser(BaseParser):
         return self._mem
 
 if __name__ == "__main__":
-    nethogs = NethogsParser("lcrail-nethogs-test.txt", "firefox")
+    nethogs = NethogsParser("lcrail-nethogs-original.txt", "lcrail")
     nethogs.parse()
-    top = TopParser("lcrail-top-test.txt", "firefox")
+    top = TopParser("lcrail-top-original.txt", "lcrail")
     top.parse()
     plt.title("CPU usage")
-    plt.plot(top.cpu)
+    plt.plot(top.timeline, top.cpu)
     plt.xlabel("Time (s)")
     plt.ylabel("Usage (%)")
     plt.show()
     plt.title("RAM usage")
     plt.xlabel("Time (s)")
     plt.ylabel("Usage (%)")
-    plt.plot(top.mem)
+    plt.plot(top.timeline, top.mem)
     plt.show()
     plt.title("Network usage")
     plt.xlabel("Time (s)")
     plt.ylabel("Usage (MB)")
-    plt.plot(nethogs.sent, label="Sent")
-    plt.plot(nethogs.received, label="Received")
+    plt.plot(nethogs.timeline, nethogs.sent, label="Sent")
+    plt.plot(nethogs.timeline, nethogs.received, label="Received")
     plt.legend()
     plt.show()
