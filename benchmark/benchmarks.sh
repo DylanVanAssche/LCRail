9b8d3f46ee0bdd5b6ff03fdc3d2685540027e871
Dylan Van Assche <dylan.van.assche@protonmail.com>
2019-04-05 11:32:32 +0200

    Clean up benchmark results of the original planner and liveboard

--- benchmark/benchmarks.sh
+++ benchmark/benchmarks.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+# Dylan Van Assche - LCRail benchmark
+# Usage: # ./benchmark.sh <benchmark name>
+
+# Add timestamp in front of the current line
 adddate() {
     while IFS= read -r line; do
         echo "$(date) $line"
     done
 }
 
-top -b | adddate >> lcrail-top-$2.txt &
+# Plot the Top CPU and RAM statistics of all processes
+top -b | adddate >> lcrail-top-$1.txt &
 P1=$!
-nethogs -v 3 -t | adddate >> lcrail-nethogs-$2.txt &
+
+# Plot the Nethogs network traffic statistics of all processes
+nethogs -v 3 -t | adddate >> lcrail-nethogs-$1.txt &
 P2=$!
+
+# Wait until both background processes are killed when we kill our script
 wait $P1 $P2
